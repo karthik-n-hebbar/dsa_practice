71f68171d90bf61ec95718d1ca582c877d8530fa
karthik-n-hebbar <karthiknh125@gmail.com>
2024-06-26 03:02:15 +0530

    Done

--- pattern_1.py
+++ pattern_1.py
@@ -1,6 +1,9 @@
 n = int(input("Enter the number: "))
 
-for i in range(1,n+1):
-    print("1"*i)
-for i in range(n-1, 0,-1):
-    print("1"*i)
+# Generate the increasing pattern
+for i in range(1, n + 1):
+    print('1' * i)
+
+# Generate the decreasing pattern
+for i in range(n - 1, 0, -1):
+    print('1' * i)
