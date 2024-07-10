d519c2fffbb5b133c48ac40bea20fba976a10e81
karthik-n-hebbar <karthiknh125@gmail.com>
2024-07-10 17:01:08 +0530

    Program 7

---
+++ sorting.py
@@ -0,0 +1,26 @@
+# QUICK SORT
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+# BUBBLE SORTING
+def bubble_sort(arr):
+    
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+user_input = input("ENTER AN ARRAY: ")
+arr = list(map(int, user_input.split()))
+sorted_arr = insertion_sort(arr)
+print(*sorted_arr)
