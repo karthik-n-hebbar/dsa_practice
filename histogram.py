048b70bf37c25ef1d522438f4070f15eca35b811
karthik-n-hebbar <karthiknh125@gmail.com>
2024-07-11 23:04:38 +0530

    Program 4

---
+++ histogram.py
@@ -0,0 +1,17 @@
+def count_characters(sentence):
+    char_count = {}
+    
+    for char in sentence:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    
+    return char_count
+
+
+sentence = input("ENTER THE SENTENCE: ")
+char_count = count_characters(sentence)
+
+for char, count in char_count.items():
+    print(f"'{char}': {count}")
